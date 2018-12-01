79e6d24ddf88d396ecf775d860f51a67298773f1
JoannChen <Q8622268@gmail.com>
2018-12-01 16:46:46 +0800

    补充提交消息代码

---
+++ app/src/main/java/com/sunny/classcome/bean/MsgBean.kt
@@ -0,0 +1,13 @@
+package com.sunny.classcome.bean
+
+/**
+ * Desc
+ * Author Justin
+ * Mail zhangye98@foxmail.com
+ * Date 2018/12/1 14:42
+ */
+data class MsgBean(
+        var type: String,
+        var title: String,
+        var time: String,
+        var content: String)
