c2e473f82a93c29a904a82bfe599630d51e48808
ZhangYe <Q8622268@gmail.com>
2018-12-13 00:19:52 +0800

    我的简介编辑信息细节调整

--- app/src/main/java/com/sunny/classcome/activity/MyProfileEditActivity.kt
+++ app/src/main/java/com/sunny/classcome/activity/MyProfileEditActivity.kt
@@ -3,6 +3,8 @@ package com.sunny.classcome.activity
 import android.view.View
 import com.sunny.classcome.R
 import com.sunny.classcome.base.BaseActivity
+import com.sunny.classcome.utils.IntentUtil
+import kotlinx.android.synthetic.main.activity_my_profile_edit.*
 
 /**
  * Desc  我的简介：编辑信息
@@ -16,8 +18,13 @@ class MyProfileEditActivity : BaseActivity() {
 
     override fun initView() {
         showTitle(titleManager.defaultTitle("编辑信息"))
+
+        txt_go_identity.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.txt_go_identity -> IntentUtil.start(this, PersonAuthActivity::class.java)
+        }
     }
 }
