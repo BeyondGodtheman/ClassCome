a68cbe17a103c320664dcacfa4dbc0d1bcbe789e
ZhangYe <Q8622268@gmail.com>
2018-12-16 16:06:16 +0800

    提交实体类

--- app/src/main/java/com/sunny/classcome/bean/ClassDetailBean.kt
+++ app/src/main/java/com/sunny/classcome/bean/ClassDetailBean.kt
@@ -1,6 +1,35 @@
 package com.sunny.classcome.bean
 
 data class ClassDetailBean(
+
         var code: String,
         var msg: String,
-        var content: ClassBean.Bean.Data?)
+        var content: ResCourseVO) {
+
+    data class ResCourseVO(
+            var id: String,
+            var createTime: String,
+            var title: String,
+            var materialList: ArrayList<ClassBean.Bean.Data.Material>,
+            var price: String,
+            var sumPrice: String,
+            var courseNum: String,
+            var personType: String,
+            var classTime: ArrayList<String>,
+            var classAddress: String,
+            var classDetailAdress: String,
+            var startTime: String,
+            var endTime: String,
+            var category: ArrayList<String>,
+            var description: String,
+            var isFavorite: String,
+            var isAppointment: String,
+            var latitude: String,
+            var longitude: String,
+            var winningBidder: String,
+            var state: String,
+            var fatherCategoryName: String,
+            var isAppraise: String,
+            var course: ClassBean.Bean.Data.Course,
+            var user: ClassBean.Bean.Data.User)
+}
---
+++ app/src/main/java/com/sunny/classcome/bean/OrderDetailBean.kt
@@ -0,0 +1,6 @@
+package com.sunny.classcome.bean
+
+data class OrderDetailBean(
+        var code: String,
+        var msg: String,
+        var content: ClassBean.Bean.Data?)
