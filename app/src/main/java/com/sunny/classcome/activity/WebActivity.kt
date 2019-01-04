6964382b3797d72f26b1a28acb039c4602e19701
ZhangYe <Q8622268@gmail.com>
2019-01-05 03:25:33 +0800

    webView适配重构

--- app/src/main/java/com/sunny/classcome/activity/WebActivity.kt
+++ app/src/main/java/com/sunny/classcome/activity/WebActivity.kt
@@ -1,39 +1,50 @@
 package com.sunny.classcome.activity
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.text.Html
 import android.view.View
 import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.sunny.classcome.R
 import com.sunny.classcome.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : BaseActivity() {
+
+    val webView:WebView by lazy {
+        WebView(this)
+    }
+
     override fun setLayout(): Int = R.layout.activity_web
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun initView() {
 
         showTitle(titleManager.defaultTitle(intent.getStringExtra("title")))
 
-//        webView.settings.javaScriptEnabled = true
-//        webView.settings.javaScriptCanOpenWindowsAutomatically = true
-//        webView.settings.allowFileAccess = true
-//        webView.settings.domStorageEnabled = true// 打开本地缓存提供JS调用,至关重要
-//        webView.settings.setAppCacheEnabled(true)
-//        webView.settings.setAppCachePath(application.cacheDir.absolutePath)
-//        webView.settings.databaseEnabled = true
-//        webView.settings.textZoom = 50
-//        webView.webViewClient = object : WebViewClient() {}
-//        webView.webChromeClient = object : WebChromeClient() {}
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.allowFileAccess = true
+        webView.settings.domStorageEnabled = true// 打开本地缓存提供JS调用,至关重要
+        webView.settings.setAppCacheEnabled(true)
+        webView.settings.setAppCachePath(application.cacheDir.absolutePath)
+        webView.settings.databaseEnabled = true
+        webView.settings.textZoom = 50
+        webView.webViewClient = object : WebViewClient() {}
+        webView.webChromeClient = object : WebChromeClient() {}
 //
         intent.getStringExtra("data")?.let {
             val html = "<html><head><meta name=\"viewport\" content=\"width=device-width,initial-scale=1, minimum-scale=1, maximum-scale=1,user-scalable=no\">" +
                     "<meta http-equiv=Content-Type content=\"text/html; charset=gb2312\"></head><body style=\"margin:0;padding:0\">" + it + "</body></html>"
-//            webView.loadDataWithBaseURL(null,html, "text/html", "gb2312",null)
-            txt_Web.text = Html.fromHtml(html)
+            webView.loadDataWithBaseURL(null,html, "text/html", "gb2312",null)
+
         }
+
+        ll_web.addView(webView)
     }
 
     override fun onClick(v: View?) {
@@ -45,5 +56,14 @@ class WebActivity : BaseActivity() {
                     .putExtra("title", title)
                     .putExtra("data", data))
         }
+    }
+
+    override fun close() {
+        ll_web.removeView(webView)
+        webView.stopLoading()
+        webView.settings.javaScriptEnabled = false
+        webView.clearHistory()
+        webView.removeAllViews()
+        webView.destroy()
     }
 }
